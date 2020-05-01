8d0cfa1e3354d043bb0f2aef56768cc413991e7d
Trần Văn Hiếu <tranhieuglpk@gmail.com>
2020-05-01 16:08:48 +0700

    edit sql

--- CHUNGKHOA_CHUYEN_DE_CNPM/frm_dat_lenh.Designer.cs
+++ CHUNGKHOA_CHUYEN_DE_CNPM/frm_dat_lenh.Designer.cs
@@ -345,7 +345,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1318, 265);
+            this.ClientSize = new System.Drawing.Size(1318, 309);
             this.Controls.Add(this.frmDatLenh_btnThayDoi);
             this.Controls.Add(this.frmDatLenh_btnDatLenh);
             this.Controls.Add(this.label11);
